e432c08a4c53cb32937fc38a0d271d69a350c53d
Ethan Winter <thanpisit1403@gmail.com>
2025-03-21 08:53:04 +0700

    updateCompany

--- src/libs/getCompany.tsx
+++ src/libs/getCompany.tsx
@@ -1,12 +1,13 @@
 export default async function getCompany(cid:string){
 
-    const response=await fetch(`http://localhost:5005/api/v1/companies/${cid}`)
-        if(!response.ok){
-            throw new Error("Failed to fetch Venues")
-        }
-        
-        const dataJson = await response.json()
-        return dataJson
+    await new Promise((resolve) => {
+        setTimeout(resolve, 300);
+    })
 
+    const response=await fetch(`http://localhost:5005/api/v1/companies/${cid}`)
     
+    if(!response.ok)
+        throw new Error("Failed to fetch Venues");
+        
+    return await response.json();
 }
