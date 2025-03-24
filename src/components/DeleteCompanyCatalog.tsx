6b4331eedf2775678a808c1b3728cd10e1d7fcd3
Sohara Mahamad <60sec.md@gmail.com>
2025-03-24 15:34:32 +0700

    fix delete company path

--- src/components/DeleteCompanyCatalog.tsx
+++ src/components/DeleteCompanyCatalog.tsx
@@ -12,7 +12,7 @@ export default async function VenueCatalog({companiesJson}: {companiesJson: Prom
         <div style={{margin:"20px", display:"flex",flexDirection:"row", alignContent:"space-around",justifyContent:"space-around", flexWrap:"wrap",padding:"20px"}}>
             {
                 companyJsonReady.data.map((companyItem)=>(
-                    <Link href={`/delete/${companyItem._id}`} className="w-1/5 px-5 py-3" key={companyItem._id}> 
+                    <Link href={`/companydelete/${companyItem._id}`} className="w-1/5 px-5 py-3" key={companyItem._id}> 
                         <Card companyName={companyItem.name} imgSrc='https://drive.google.com/uc?export=view&id=1VThaI32ox5b6Knz9AHAf9IN3ek6RIWrH'/>
                     </Link>
                 ))
